faa6402351bc4b59fbac434a2db19a80c2d11769
yarinbnyamin <bnyamin@post.bgu.ac.il>
2022-08-02 15:05:50 +0300

    fix error while build bug

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   configureWebpack: {
-    devtool: "source-map"
+    devtool: "source-map",
   },
   devServer: {
     // host: "132.72.65.211"
-    host: "localhost"
-  }
+    host: "localhost",
+  },
   // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
 };
