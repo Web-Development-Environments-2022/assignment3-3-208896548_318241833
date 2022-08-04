f577783170f870365b62aa4c3646bab3b04d399b
yarinbnyamin <bnyamin@post.bgu.ac.il>
2022-08-04 17:08:56 +0300

    new site version

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 const state = {
-  server_domain: "http://localhost:3000",
+  // server_domain: "http://localhost:3000",
 
-  // server_domain: "http://yumtum.cs.bgu.ac.il/", // 132.72.65.211
+  server_domain: "http://yumtum.cs.bgu.ac.il", // 132.72.65.211
 };
